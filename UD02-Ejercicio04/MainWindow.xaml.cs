77133cbf3437d9dec91c9ba37367f92cb5d02e08
Francisco Julian Gil Marin <fj.gil16@iesdoctorbalmis.com>
2020-10-24 13:46:17 +0200

    Agregar archivos de proyecto.

---
+++ UD02-Ejercicio04/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UD02_Ejercicio04
+{
+    public partial class MainWindow : Window
+    {
+        private const int NUMERO_MAXIMO_CARACTERES = 140;
+        public MainWindow()
+        {
+            InitializeComponent();
+            contadorTextBlock.Text = $"{escribirTextBox.Text.Length}/{NUMERO_MAXIMO_CARACTERES}";
+        }
+
+        private void EscribirTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (escribirTextBox.Text.Length >= NUMERO_MAXIMO_CARACTERES)
+            {
+                escribirTextBox.Text = escribirTextBox.Text.Substring(0, NUMERO_MAXIMO_CARACTERES);
+                escribirTextBox.IsReadOnly = true;
+            }
+
+            contadorTextBlock.Text = $"{escribirTextBox.Text.Length}/{NUMERO_MAXIMO_CARACTERES}";
+        }
+    }
+}
